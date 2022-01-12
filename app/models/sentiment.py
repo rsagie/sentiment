5df6bc81be20d7d16446851578eab21ac6c17aae
Roey Sagie <roey@oneai.com>
2022-01-12 12:35:37 +0200

    use type:custome and name: sentiment , to alignwith UI

--- app/models/sentiment.py
+++ app/models/sentiment.py
@@ -13,15 +13,14 @@ async def get_sentiment(input_text):
     # input_text = "Sure it looks line it has some intersting feature"
     analysis = TextBlob(input_text)
     print(analysis.sentiment.polarity)
-    sentiment = ""
     if analysis.sentiment.polarity > 0:
         sentiment = "positive"
     else:
         sentiment = "negative"
 
-    label = Label(type="sentiment",name=None, span=[0, len(input_text)-1], value=sentiment,
-                  output_spans=[{"section": 0, "start": 0, "end": len(input_text)-1}],
-                  input_spans=None, span_text=None)
+    label = Label(type="custom", name="sentiment", span=None, value=sentiment,
+                                 output_spans=[],
+                                 input_spans=None, span_text=None)
 
     skill_output: SkillOutput = SkillOutput(labels=[label])
 
